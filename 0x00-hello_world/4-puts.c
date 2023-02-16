77df138e7b89c61d4eaea559970d499db1412e4d
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-02-16 06:15:43 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * return 0.
+ * main - prints "Programming is like building a multilingual
+ *  puzzle, followed by a newline
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,5 +12,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	retun(0);
+	return(0);
 }
