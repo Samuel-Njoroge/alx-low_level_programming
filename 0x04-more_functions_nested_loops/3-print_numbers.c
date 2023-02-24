d1377821b87ae53294a6dbecf9056a4cc4c90c07
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-02-24 11:44:02 -0800

    printing numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - prints between 0 and 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
