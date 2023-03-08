5a851dd398cca902c397d325675af77e7589ca37
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-03-08 00:41:08 -0800

    square root recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the string length
- * @s : string
- * Return : string length
+ * _strlen_recursion - returns the length of a string.
+ *@s: the string we are t find the length of.
+ *Return: the length of the string.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	return (0);
+		return (0);
 	else
-	return (1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
